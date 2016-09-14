b2746714a7f96033707e37896f65efa570df46ff
Stewart Crainie <stewartcrainie@gmail.com>
2016-09-14 12:12:48 +0800

    Show View

--- SCPopDatePicker/ViewController.swift
+++ SCPopDatePicker/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController, SCPopDatePickerDelegate {
         self.datePicker.btnColour = UIColor.blackColor()
         self.datePicker.showCornerRadius = false
         self.datePicker.delegate = self
-        
+        self.datePicker.show(attachToView: self.view)
     }
     
     func scPopDatePickerDidSelectDate(date: NSDate) {
